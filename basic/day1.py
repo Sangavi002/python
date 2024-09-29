1b474f815beba68d8a311a694b80ee955b854a50
sangavi002 <sangavibabu007@gmail.com>
2024-09-29 15:08:58 +0530

    python basic

---
+++ basic/day1.py
@@ -0,0 +1,41 @@
+# print("Welcome to python")
+
+# INPUT - takes the input from the user and converts it into a string.
+# val = input("Enter your value: ") 
+# print(val) 
+
+# a = 9
+# b = 4
+# add = a + b 
+
+# sub = a - b 
+
+# mul = a * b 
+
+# mod = a % b 
+
+# p = a ** b 
+# print(add) 
+# print(sub) 
+# print(mul) 
+# print(mod) 
+# print(p)
+
+# i = 25
+# if (i == 25):
+#     print("i is 10")
+# elif (i == 20):
+#     print("i is 15")
+# elif (i > 20):
+#     print("i is 20")
+# else:
+#     print("i is not present")
+
+# count = 0
+# while (count < 3):
+#     print("Hello Geek")
+#     count += 1
+
+# count = 0
+# while (count == 0):
+#     print("Hello Geek")
